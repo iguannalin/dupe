479982424c0eb312d61209b5f44bf287f56e7557
seasons986 <annalin12345@gmail.com>
2023-10-09 02:18:13 -0400

    meta tag didn't work, going back to window open

--- index.js
+++ index.js
@@ -1,36 +1,19 @@
 window.addEventListener("load", () => {
-  function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
-  }
-
   function display() {
-    const slashes = window.location.hash;
-    console.log(window.location);
-    if (slashes) console.log({slashes});
-    const slash = 1;
-    const meta = document.createElement("meta");
-    meta.httpEquiv = "REFRESH";
-    meta.content = "5";
+    let title = Number(document.querySelector("title").innerText);
+    if (!title) title = 1; 
+    else title+=1;
 
-    const url = new URL("https://iguannalin.github.io/dupe");
-    url.hash = `#${slashes ? slashes++ : 1}`
-    // params.append("q", slash);
-    console.log({url});
-    meta.url = url;
-    // link.onclick = () => {
-      // <meta HTTP-EQUIV="REFRESH" CONTENT="1; URL="></meta>
-      document.head.appendChild(meta);
+    const container = document.getElementById("container");
+    const p = document.createElement("p");
+    p.innerText = Math.random()>0.5 ? "/" : "\\";
+    container.appendChild(p);
 
-      // const text = `<!doctypehtml><title>how-to live with spam</title><meta charset=utf-8><meta content="width=device-width,initial-scale=1"name=viewport><link href=https://iguannalin.github.io/spam/index.css rel=stylesheet><script src=https://iguannalin.github.io/spam/spam.js></script><div id=container></div>`;
-      // const blob = new Blob([text], {type: "text/html"});
-      // const blobUrl = URL.createObjectURL(blob);
-      // window.open(blobUrl, "_self", `popup,location,status,scrollbars,resizable,width=400, height=400, left=${getRandomInt(0,500)}, top=${getRandomInt(0,500)}`);
-      // window.URL.revokeObjectURL(blobUrl);
-
-    // }
-    // document.body.appendChild(link);
+    const text = `<!doctypehtml><title>${title}</title><meta charset=utf-8><meta content="width=device-width,initial-scale=1"name=viewport><link href=https://iguannalin.github.io/dupe/index.css rel=stylesheet><script src=https://iguannalin.github.io/dupe/index.js></script><div id=container></div>`;
+    const blob = new Blob([text], {type: "text/html"});
+    const blobUrl = URL.createObjectURL(blob);
+    window.open(blobUrl, "_self", "location,status,scrollbars,resizable");
+    window.URL.revokeObjectURL(blobUrl);
   }
 
   display();
